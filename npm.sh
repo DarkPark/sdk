22b03751056cbe13989b7edcdaa8bd518a0222f6
DarkPark <darkpark@pisem.net>
2015-12-28 18:48:46 +0200

    add npm publish script

---
+++ npm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# repositories
+organizations=(cjs spa stb)
+cjs=(async emitter format model parse-query parse-uri wamp)
+spa=(app develop gettext gulp keys router)
+stb=(app develop gulp keys shim-classlist shim-bind shim-frame referrer)
+
+for organization in ${organizations[@]}; do
+    echo -e "\e[1m\e[32m[$organization]\e[0m"
+    repos=$organization[@]
+
+    # prepare
+    cd $organization &&
+
+    # get all organization repositories
+    for name in ${!repos}; do
+        npm publish $name 2> /dev/null || (echo "  $name skipped" && rm npm-debug.log)
+    done &&
+
+    cd ..
+done
