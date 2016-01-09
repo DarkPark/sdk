e0c27600f5fb449ba60c736db7e6d8c073a759e3
DarkPark <darkpark@pisem.net>
2016-01-09 14:31:31 +0200

    add gulp-jade repo

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # repositories
 organizations=(cjs spa stb)
 cjs=(async emitter format model parse-query parse-uri wamp)
-spa=(app boilerplate component component-page develop dom gettext gulp keys preloader request router)
+spa=(app boilerplate component component-page develop dom gettext gulp gulp-jade keys preloader request router)
 stb=(app boilerplate develop gulp keys shim-classlist shim-bind shim-frame referrer)
 
 for organization in ${organizations[@]}; do
