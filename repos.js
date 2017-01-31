0cef8fc767f9b0bc5d0235821ba898612ec76dda
Stanislav Kalashnik <darkpark.main@gmail.com>
2017-01-31 12:38:38 +0200

    move repos to a separate file and rework clone/push/sass to run serial

---
+++ repos.js
@@ -0,0 +1,120 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+/* eslint quote-props: off */
+
+'use strict';
+
+// public
+module.exports = {
+    cjssdk: {
+        'async':            {name: 'cjs-async', url: 'git@github.com:cjssdk/async.git'},
+        'emitter':          {name: 'cjs-emitter', url: 'git@github.com:cjssdk/emitter.git'},
+        'eslint-config':    {name: 'cjs-eslint-config', url: 'git@github.com:cjssdk/eslint-config.git'},
+        'format':           {name: 'cjs-format', url: 'git@github.com:cjssdk/format.git'},
+        'gettext':          {name: 'cjs-gettext', url: 'git@github.com:cjssdk/gettext.git'},
+        'iso-639':          {name: 'cjs-iso-639', url: 'git@github.com:cjssdk/iso-639.git'},
+        'model':            {name: 'cjs-model', url: 'git@github.com:cjssdk/model.git'},
+        'property-watcher': {name: 'cjs-property-watcher', url: 'git@github.com:cjssdk/property-watcher.git'},
+        'query':            {name: 'cjs-query', url: 'git@github.com:cjssdk/query.git'},
+        'runner':           {name: 'cjs-runner', url: 'git@github.com:cjssdk/runner.git'},
+        'uri':              {name: 'cjs-uri', url: 'git@github.com:cjssdk/uri.git'},
+        'wamp':             {name: 'cjs-wamp', url: 'git@github.com:cjssdk/wamp.git'}
+    },
+    magsdk: {
+        'app':                   {name: 'mag-app', url: 'git@192.168.1.222:/web/magsdk/app.git'},
+        'boilerplate':           {name: null, url: 'git@192.168.1.222:/web/magsdk/boilerplate.git'},
+        'data-cacher':           {name: 'mag-data-cacher', url: 'git@192.168.1.222:/web/magsdk/data-cacher.git'},
+        'eslint-config':         {name: 'mag-eslint-config', url: 'git@github.com:magsdk/eslint-config.git'},
+        'component-check-list':  {name: 'mag-component-check-list', url: 'git@192.168.1.222:/web/magsdk/component-check-list.git'},
+        'component-footer':      {name: 'mag-component-footer', url: 'git@192.168.1.222:/web/magsdk/component-footer.git'},
+        'component-radio-list':  {name: 'mag-component-radio-list', url: 'git@192.168.1.222:/web/magsdk/component-radio-list.git'},
+        'component-layout':      {name: 'mag-component-layout', url: 'git@192.168.1.222:/web/magsdk/component-layout.git'},
+        'component-layout-list': {name: 'mag-component-layout-list', url: 'git@192.168.1.222:/web/magsdk/component-layout-list.git'},
+        'component-list':        {name: 'mag-component-list', url: 'git@192.168.1.222:/web/magsdk/component-list.git'},
+        'component-modal':       {name: 'mag-component-modal', url: 'git@192.168.1.222:/web/magsdk/component-modal.git'},
+        'component-panel':       {name: 'mag-component-panel', url: 'git@192.168.1.222:/web/magsdk/component-panel.git'},
+        'component-panel-set':   {name: 'mag-component-panel-set', url: 'git@192.168.1.222:/web/magsdk/component-panel-set.git'},
+        'component-scroll-area': {name: 'mag-component-scroll-area', url: 'git@192.168.1.222:/web/magsdk/component-scroll-area.git'},
+        'component-value-list':  {name: 'mag-component-value-list', url: 'git@192.168.1.222:/web/magsdk/component-value-list.git'},
+        'plugin-css':            {name: 'mag-plugin-css', url: 'git@192.168.1.222:/web/magsdk/plugin-css.git'},
+        'magsdk':                {name: 'magsdk', url: 'git@192.168.1.222:/web/magsdk/magsdk.git'}
+    },
+    magcore: {
+        'stalker-apps-base':  {name: 'stalker-apps-base', url: 'git@github.com:magcore/stalker-apps-base.git'},
+        'stalker-apps-extra': {name: 'stalker-apps-extra', url: 'git@github.com:magcore/stalker-apps-extra.git'},
+        'platforms':          {name: 'magcore-platforms', url: 'git@192.168.1.222:/web/magcore/platforms.git'}
+    },
+    spasdk: {
+        'app':                    {name: 'spa-app', url: 'git@github.com:spasdk/app.git'},
+        'boilerplate':            {name: null, url: 'git@github.com:spasdk/boilerplate.git'},
+        'component':              {name: 'spa-component', url: 'git@github.com:spasdk/component.git'},
+        'component-button':       {name: 'spa-component-button', url: 'git@github.com:spasdk/component-button.git'},
+        'component-checkbox':     {name: 'spa-component-checkbox', url: 'git@github.com:spasdk/component-checkbox.git'},
+        'component-flicker':      {name: 'spa-component-flicker', url: 'git@github.com:spasdk/component-flicker.git'},
+        'component-grid':         {name: 'spa-component-grid', url: 'git@github.com:spasdk/component-grid.git'},
+        'component-input':        {name: 'spa-component-input', url: 'git@github.com:spasdk/component-input.git'},
+        'component-list':         {name: 'spa-component-list', url: 'git@github.com:spasdk/component-list.git'},
+        'component-modal':        {name: 'spa-component-modal', url: 'git@github.com:spasdk/component-modal.git'},
+        'component-page':         {name: 'spa-component-page', url: 'git@github.com:spasdk/component-page.git'},
+        'component-panel':        {name: 'spa-component-panel', url: 'git@github.com:spasdk/component-panel.git'},
+        'component-progress-bar': {name: 'spa-component-progress-bar', url: 'git@github.com:spasdk/component-progress-bar.git'},
+        'component-scrollbar':    {name: 'spa-component-scrollbar', url: 'git@github.com:spasdk/component-scrollbar.git'},
+        'component-tab':          {name: 'spa-component-tab', url: 'git@github.com:spasdk/component-tab.git'},
+        'component-widget':       {name: 'spa-component-widget', url: 'git@github.com:spasdk/component-widget.git'},
+        'dom':                    {name: 'spa-dom', url: 'git@github.com:spasdk/dom.git'},
+        'eslint-config':          {name: 'spa-eslint-config', url: 'git@github.com:spasdk/eslint-config.git'},
+        'gettext':                {name: 'spa-gettext', url: 'git@github.com:spasdk/gettext.git'},
+        'keys':                   {name: 'spa-keys', url: 'git@github.com:spasdk/keys.git'},
+        'plugin':                 {name: 'spa-plugin', url: 'git@github.com:spasdk/plugin.git'},
+        'plugin-css':             {name: 'spa-plugin-css', url: 'git@github.com:spasdk/plugin-css.git'},
+        'plugin-eslint':          {name: 'spa-plugin-eslint', url: 'git@github.com:spasdk/plugin-eslint.git'},
+        'plugin-gettext':         {name: 'spa-plugin-gettext', url: 'git@github.com:spasdk/plugin-gettext.git'},
+        'plugin-jade':            {name: 'spa-plugin-jade', url: 'git@github.com:spasdk/plugin-jade.git'},
+        'plugin-livereload':      {name: 'spa-plugin-livereload', url: 'git@github.com:spasdk/plugin-livereload.git'},
+        'plugin-sass':            {name: 'spa-plugin-sass', url: 'git@github.com:spasdk/plugin-sass.git'},
+        'plugin-ssh':             {name: 'spa-plugin-ssh', url: 'git@github.com:spasdk/plugin-ssh.git'},
+        'plugin-static':          {name: 'spa-plugin-static', url: 'git@github.com:spasdk/plugin-static.git'},
+        'plugin-wamp':            {name: 'spa-plugin-wamp', url: 'git@github.com:spasdk/plugin-wamp.git'},
+        'plugin-webpack':         {name: 'spa-plugin-webpack', url: 'git@github.com:spasdk/plugin-webpack.git'},
+        'plugin-zip':             {name: 'spa-plugin-zip', url: 'git@github.com:spasdk/plugin-zip.git'},
+        'preloader':              {name: 'spa-preloader', url: 'git@github.com:spasdk/preloader.git'},
+        'request':                {name: 'spa-request', url: 'git@github.com:spasdk/request.git'},
+        'spasdk':                 {name: 'spasdk', url: 'git@github.com:spasdk/spasdk.git'},
+        'wamp':                   {name: 'spa-wamp', url: 'git@github.com:spasdk/wamp.git'},
+        'webui':                  {name: 'spa-webui', url: 'git@github.com:spasdk/webui.git'}
+    },
+    stbsdk: {
+        'app':                 {name: 'stb-app', url: 'git@github.com:stbsdk/app.git'},
+        'boilerplate':         {name: null, url: 'git@github.com:stbsdk/boilerplate.git'},
+        'referrer':            {name: 'stb-referrer', url: 'git@github.com:stbsdk/referrer.git'},
+        'eslint-config':       {name: 'stb-eslint-config', url: 'git@github.com:stbsdk/eslint-config.git'},
+        'component':           {name: 'stb-component', url: 'git@github.com:stbsdk/component.git'},
+        'component-button':    {name: 'stb-component-button', url: 'git@github.com:stbsdk/component-button.git'},
+        'component-checkbox':  {name: 'stb-component-checkbox', url: 'git@github.com:stbsdk/component-checkbox.git'},
+        'component-grid':      {name: 'stb-component-grid', url: 'git@github.com:stbsdk/component-grid.git'},
+        'component-input':     {name: 'stb-component-input', url: 'git@github.com:stbsdk/component-input.git'},
+        'component-list':      {name: 'stb-component-list', url: 'git@github.com:stbsdk/component-list.git'},
+        'component-modal':     {name: 'stb-component-modal', url: 'git@github.com:stbsdk/component-modal.git'},
+        'component-page':      {name: 'stb-component-page', url: 'git@github.com:stbsdk/component-page.git'},
+        'component-panel':     {name: 'stb-component-panel', url: 'git@github.com:stbsdk/component-panel.git'},
+        'component-scrollbar': {name: 'stb-component-scrollbar', url: 'git@github.com:stbsdk/component-scrollbar.git'},
+        'shim-classlist':      {name: 'stb-shim-classlist', url: 'git@github.com:stbsdk/shim-classlist.git'},
+        'shim-bind':           {name: 'stb-shim-bind', url: 'git@github.com:stbsdk/shim-bind.git'},
+        'shim-frame':          {name: 'stb-shim-frame', url: 'git@github.com:stbsdk/shim-frame.git'},
+        'stbsdk':              {name: 'stbsdk', url: 'git@github.com:stbsdk/stbsdk.git'},
+        'keys':                {name: 'stb-keys', url: 'git@github.com:stbsdk/keys.git'},
+        'plugin-css':          {name: 'stb-plugin-css', url: 'git@github.com:stbsdk/plugin-css.git'},
+        'plugin-proxy':        {name: 'stb-plugin-proxy', url: 'git@github.com:stbsdk/plugin-proxy.git'},
+        'plugin-sass':         {name: 'stb-plugin-sass', url: 'git@github.com:stbsdk/plugin-sass.git'},
+        'plugin-ssh':          {name: 'stb-plugin-ssh', url: 'git@github.com:stbsdk/plugin-ssh.git'},
+        'plugin-webpack':      {name: 'stb-plugin-webpack', url: 'git@github.com:stbsdk/plugin-webpack.git'},
+        'plugin-weinre':       {name: 'stb-plugin-weinre', url: 'git@github.com:stbsdk/plugin-weinre.git'}
+    },
+    tznsdk: {
+        'app':  {name: 'tzn-app',  url: 'git@192.168.1.222:/web/tznsdk/app.git'},
+        'keys': {name: 'tzn-keys', url: 'git@192.168.1.222:/web/tznsdk/keys.git'}
+    }
+};
